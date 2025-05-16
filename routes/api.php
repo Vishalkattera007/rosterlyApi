4bf42659a0a4e9d6e75eaf0e69ca24c276402fd3
Glansa-Solutions <glansa.solutions777@gmail.com>
2025-05-16 13:12:15 +0530

    logged in user fetch

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RolesController;
 use App\Http\Controllers\Api\LocationController;
-use App\Http\Controllers\Api\UserProfileController;
+use App\Http\Controllers\Api\RolesController;
 use App\Http\Controllers\Api\UnavailabilityController;
+use App\Http\Controllers\Api\UserProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserProfileController::class, 'login']);
 
 Route::middleware('apiauth')->group(function () {
     Route::post('/logout', [UserProfileController::class, 'logout']);
     Route::prefix('/users')->group(function () {
-        Route::get('/', [UserProfileController::class, 'index']);
-        Route::post('/users', [UserProfileController::class, 'store'])->name('users.store');
-        Route::get('/role/{id}', [UserProfileController::class, 'show'])->name('users.show');
+        Route::get('/{id?}', [UserProfileController::class, 'index']);
+        Route::post('/', [UserProfileController::class, 'store'])->name('users.store');
         Route::put('{id}', [UserProfileController::class, 'update']);
-        Route::get('/login/{loginId?}', [UserProfileController::class, 'getUsersCreatedBy']);
-
         Route::delete('{id}', [UserProfileController::class, 'destroy']);
+        Route::get('/role/{id}', [UserProfileController::class, 'show'])->name('users.show');
+        Route::get('/login/{loginId?}', [UserProfileController::class, 'getUsersCreatedBy']);
     });
 
     Route::get('/roles', [RolesController::class, 'index'])->name('roles.index');
